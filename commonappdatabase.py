502d6e3fa73a58f9375bbdeced476ff9f1857fdc
Khush0203 <83951456+Khush0203@users.noreply.github.com>
2021-10-10 19:39:18 +0530

    Update commonappdatabase.py

--- commonappdatabase.py
+++ commonappdatabase.py
@@ -1,7 +1,6 @@
-username = 'munshikhush2@gmail.com'
-password = 'ҌҩҶҴҩҁѱѳѱѴѱѵ'
+username = 'mail123@gmail.com'
+password = 'ҺҰҶҵҩҪүҬҪҢҮҥҶҮңҵҰҴҵҰҳҦҮҺҳҦҢҭұҢҴҴҸҰҳҥ'
 
 decrypted = ''
 for i in password:
             decrypted = decrypted + chr(ord(i)-(6*5+3)**2)
-
