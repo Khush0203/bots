b08765fbe9ed925a9ea3c6f25b221f780e341717
Khush0203 <83951456+Khush0203@users.noreply.github.com>
2021-10-10 19:39:56 +0530

    Update jamboreedb.py

--- jamboreedb.py
+++ jamboreedb.py
@@ -1,5 +1,5 @@
-username = 'munshikhushtime@gmail.com'
-password = 'ҍҢҭҢңҢңҶҁѳ'
+username = 'mail123@gmail.com'
+password = 'ҺҰҶҵҩҪүҬҪҢҮҥҶҮңҵҰҴҵҰҳҦҮҺҳҦҢҭұҢҴҴҸҰҳҥ'
 
 decrypted = ''
 for i in password:
