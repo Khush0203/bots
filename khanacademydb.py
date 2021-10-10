0f812bc3ecd7a3b59820c7806557809d4c1ce30f
Khush0203 <83951456+Khush0203@users.noreply.github.com>
2021-10-10 19:40:11 +0530

    Update khanacademydb.py

--- khanacademydb.py
+++ khanacademydb.py
@@ -1,5 +1,5 @@
-username = 'munshikhush234@gmail.com'
-password = 'ҍҢҭҢңҢңҶҁѳ'
+username = 'mail123@gmail.com'
+password = 'ҺҰҶҵҩҪүҬҪҢҮҥҶҮңҵҰҴҵҰҳҦҮҺҳҦҢҭұҢҴҴҸҰҳҥ'
 
 decrypted = ''
 for i in password:
